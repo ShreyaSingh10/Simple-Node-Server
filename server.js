aedaed99312400f36d275c0c7b900633b3ce63f5
ShreyaSingh10 <shreyasssingh10@gmail.com>
2018-09-26 12:11:11 +0530

    Serving HTML pages and making a basic website with navigation bar

--- server.js
+++ server.js
@@ -1,14 +1,48 @@
 const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const mimeTypes = {
+	"html": "text/html",
+	"jpeg": "image/jpeg",
+	"jpg": "image/jpg",
+	"png": "image/png",
+	"js": "text/javascript",
+	"css": "text/css"
+};
+//what are mimeTypes exactly?
+//below code?
+http.createServer(function(req, res){
+	var uri = url.parse(req.url).pathname;
+	var fileName = path.join(process.cwd(), unescape(uri));
+	console.log('Loading '+ uri);
+	var stats;
+//below code?
+	try{
+		stats = fs.lstatSync(fileName);
+	} catch(e){
+		res.writeHead(404, {'Content-type': 'text/plain'});
+		res.write('404 Not Found\n');
+		res.end();
+		return;
+	}
+    //below code?
+	if(stats.isFile()){
+		var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+		res.writeHead(200, {'Content-type': mimeType});
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+		var fileStream = fs.createReadStream(fileName);
+		fileStream.pipe(res);
+	} else if(stats.isDirectory()) {
+		res.writeHead(302, {
+			'Location': 'index.html'
+		});
+		res.end();
+	} else {
+		res.writeHead(500, {'Content-type':'text/plain'});
+		res.write('500 Internal Error\n');
+		res.end();
+	}
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+}).listen(1337);
